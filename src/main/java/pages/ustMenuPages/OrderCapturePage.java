246e8affd78c360bba71b44a029d0077247ba563
SOLADM <utku.ocak@consultant.turkcell.com.tr>
2018-05-09 09:35:22 +0300

    sipariş devam

--- src/main/java/pages/ustMenuPages/OrderCapturePage.java
+++ src/main/java/pages/ustMenuPages/OrderCapturePage.java
@@ -1,9 +1,6 @@
 package pages.ustMenuPages;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import pages.MainPage;
@@ -12,6 +9,7 @@ import pages.pageData.UstMenuData;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class OrderCapturePage extends MainPage {
 
@@ -43,11 +41,13 @@ public class OrderCapturePage extends MainPage {
     public class Fiber extends MainPage {
 
         private SelenideElement TXT_KAMPANYAARA = $(By.id(GetObject("MAYA", "TXT_SEARCHPRODUCT_ID", "ID", "MayaOrderCapturePage", "PRP")));
-        private SelenideElement BTN_DEGISTIR = $(By.xpath(GetObject("MAYA","BTN_DEGISTIRFORLOC_ID","XPATH","MayaOrderCapturePage","PRP")));
-        private  SelenideElement TXT_BINALOKASYON = $(By.id(GetObject("MAYA","TXT_LOCATION_ID","ID","MayaOrderCapturePage","PRP")));
-        private SelenideElement BTN_ARA = $(By.xpath(GetObject("MAYA","BTN_SEARCHLOCATION","XPATH","MayaOrderCapturePage","PRP")));
-        private SelenideElement BTN_LOKASYONSEC = $(By.xpath(GetObject("MAYA","BTN_LOCATIONRESULT","XPATH","MayaOrderCapturePage","PRP")));
-
+        private SelenideElement BTN_DEGISTIR = $(By.xpath(GetObject("MAYA", "BTN_DEGISTIRFORLOC_ID", "XPATH", "MayaOrderCapturePage", "PRP")));
+        private SelenideElement TXT_BINALOKASYON = $(By.id(GetObject("MAYA", "TXT_LOCATION_ID", "ID", "MayaOrderCapturePage", "PRP")));
+        private SelenideElement BTN_ARA = $(By.xpath(GetObject("MAYA", "BTN_SEARCHLOCATION", "XPATH", "MayaOrderCapturePage", "PRP")));
+        private SelenideElement BTN_LOKASYONSEC = $(By.id(GetObject("MAYA", "BTN_LOCATIONRESULT", "ID", "MayaOrderCapturePage", "PRP")));
+        private SelenideElement TXT_DAIRE_ID = $(By.id(GetObject("MAYA", "TXT_DAIRE_ID", "ID", "MayaOrderCapturePage", "PRP")));
+        private SelenideElement BTN_NEXTDAIRE_XPATH = $(By.xpath(GetObject("MAYA","BTN_NEXTDAIRE_XPATH","XPATH","MayaOrderCapturePage","PRP")));
+        private SelenideElement BTN_LOCATIONKAYDET_ID = $(By.id(GetObject("MAYA","BTN_LOCATIONKAYDET_ID","ID","MayaOrderCapturePage","PRP")));
 
         ElementsCollection TBL_KAMPANYA = $$("tbody[id='productSelectionWizardForm:fiberOfferDataTable_data'] tr[data-ri]");
         ElementsCollection TBL_LOKASYON = $$("tbody[id='productSelectionWizardForm:fiberAddressManagementForm:fiberAddressTableLazy_data'] tr[data-ri]");
@@ -79,19 +79,41 @@ public class OrderCapturePage extends MainPage {
 
         @Step("Lokasyon tablosundan lokasyon seçilir.")
         public Fiber tablodanLokasyonSec() {
-            TBL_LOKASYON.
-            shouldBe(CollectionCondition.sizeGreaterThan(0)).first().click();
+            TBL_LOKASYON
+                    .first()
+                    .$("td:nth-child(1)")
+                    .click();
 
             return this;
         }
 
-        @Step("Değiştir butonu tıklanır.")
+        @Step("Lokasyon id alanına \"{lokasyonId}\" yazılır.")
         public Fiber lokasyonIDDoldur(String lokasyonId) {
             TXT_BINALOKASYON.sendKeys(lokasyonId);
+            sleep(1000);
             return this;
         }
 
+        @Step("Daire no alanına \"{daireNo}\" yazılır.")
+        public Fiber daireNoDoldur(String daireNo) {
+            TXT_DAIRE_ID.sendKeys(daireNo);
+            return this;
+        }
 
+        @Step("Daire no seçilir. \"{daireNo}\" ")
+        public Fiber daireNoSec(String daireNo) {
+            while (!WebDriverRunner.getWebDriver().findElement(By.xpath("//span[text()='" + daireNo + "']")).isDisplayed()) {
+                BTN_NEXTDAIRE_XPATH.click();
+            }
+            WebDriverRunner.getWebDriver().findElement(By.xpath("//span[text()='" + daireNo + "']")).click();
+            return this;
+        }
+
+        @Step("Kaydet butonuna tıklanır.")
+        public Fiber kaydet() {
+            BTN_LOKASYONSEC.click();
+            return this;
+        }
 
         @Step("Kampanya Ara alanına \"{kampanya}\" yazılır.")
         public Fiber kampanyaAra(String kampanya) {
