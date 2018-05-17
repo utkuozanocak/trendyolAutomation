3e41181966b283789217ede7fd7c617d1a13d71a
Zehra <zehra.yildiz@consultant.turkcell.com.tr>
2018-05-17 08:24:55 +0300

    paket yapısı

--- src/test/java/tests/MayaTestAdsl/AdslTests.java
+++ src/test/java/tests/MayaTestAdsl/AdslTests.java
@@ -1,4 +1,4 @@
-package tests.testMaya;
+package tests.MayaTestAdsl;
 
 import common.BaseTest;
 import data.TestDataMaya;
@@ -7,8 +7,8 @@ import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.MainPageMaya;
-import pages.ustMenuPagesMaya.AdresBilgileriPage;
 import pages.ustMenuPagesMaya.OrderCapturePage;
+import pages.MayaTest;
 
 public class AdslTests extends BaseTest {
     MayaTest mayaTest = new MayaTest();
